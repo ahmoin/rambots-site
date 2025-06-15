b6d3e6591bb41b5cdadf35ecd91e589dd9768764
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-06-14 22:06:12 -0400

    refactor: remove grid and background glows on root page

--- src/app/(app)/(root)/page.tsx
+++ src/app/(app)/(root)/page.tsx
@@ -13,10 +13,6 @@ const _description =
 export default function IndexPage() {
 	return (
 		<div className="@container/page flex flex-1 flex-col gap-8 p-6">
-			<div className="absolute inset-0 h-96 md:h-auto 2xl:h-200 [background-size:40px_40px] [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)] dark:[background-image:linear-gradient(to_right,#141a30_1px,transparent_1px),linear-gradient(to_bottom,#141a30_1px,transparent_1px)] -z-30" />
-			<div className="absolute top-0 right-0 bottom-0 left-0 h-96 md:h-auto 2xl:h-200 lg bg-[radial-gradient(circle_800px_at_0%_800px,rgba(12,12,245,0.5),transparent)] -z-20"></div>
-			<div className="absolute top-0 right-0 bottom-0 left-0 h-96 md:h-auto 2xl:h-200 bg-[radial-gradient(circle_800px_at_100%_200px,rgba(12,12,245,0.5),transparent)] -z-20"></div>
-			<div className="pointer-events-none absolute inset-0 h-96 md:h-auto 2xl:h-200 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] -z-10"></div>
 			<div className="flex flex-row gap-8 mt-16 -mb-16 md:-mb-0">
 				<div className="w-full max-w-3xl mx-auto text-center flex flex-col justify-between">
 					{/* <div className="w-1/2 min-w-2xl text-left flex flex-col justify-between"> */}
